534555909bd15fdd7d9e72655f3600a8d90821a6
heitorpagotto <58671082+heitorpagotto@users.noreply.github.com>
2025-05-13 22:15:12 -0300

    started rsa

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,91 @@
+import random
+
+
+class RsaCryptography:
+    _privateKey = []
+    _publicKey = []
+
+    @staticmethod
+    def _is_prime(n):
+        if n == 1:
+            return True
+
+        for i in range(2, n):
+            if (n % i) == 0:
+                return False
+
+        return True
+
+    @staticmethod
+    def _mod_inverse(e, delta):
+        for d in range(3, delta):
+            if (d * e) % delta == 1:
+                return d
+
+        raise ValueError("Inverse does not exist")
+
+    def _generate_prime_numbers(self, start, end):
+        primes = []
+        for i in range(start, end):
+            if self._is_prime(i):
+                primes.append(i)
+
+        return primes
+
+    def encrypt(self, message):
+        # 1 - E
+        # 0 - N
+        # print(self.pu)
+        encoded_message = [ord(char) for char in message]
+        encrypted_message = [pow(ch, self._publicKey[1], self._publicKey[0]) for ch in encoded_message]
+
+        return encrypted_message
+
+    def decrypt(self, encrypted_message):
+        # 1 - D
+        # 0 - N
+        encoded_message = [pow(ch, self._privateKey[1], self._privateKey[0]) for ch in encrypted_message]
+        decrypted_message = "".join(chr(cha) for cha in encoded_message)
+
+        return decrypted_message
+
+    def calculate_rsa_variables(self):
+        initial_primes = self._generate_prime_numbers(1000, 10000)
+
+        p = initial_primes[random.randint(0, len(initial_primes) - 1)]
+
+        initial_primes = self._generate_prime_numbers(p + 1, 10000)
+
+        q = initial_primes[random.randint(0, len(initial_primes) - 1)]
+
+        # p = 3
+        # q = 11
+
+        while q == p:
+            q = initial_primes[random.randint(0, len(initial_primes) - 1)]
+
+        n = p * q
+        delta = (p - 1) * (q - 1)
+
+        print("P= ", p)
+        print("Q= ", q)
+        print("N= ", n)
+        print("Delta= ", delta)
+
+        initial_primes = self._generate_prime_numbers(q + 1, delta)
+
+        #aux = "DIGITE UM NUMERO mdc(" + str(delta) + ",e) =1 ; 1 < e < " + str(delta) + ":"
+       # e = initial_primes[random.randint(0, len(initial_primes) - 1)]
+        e = 17
+        d = self._mod_inverse(e, delta)
+
+        print("E= ", e)
+        print("D= ", d)
+
+        self._privateKey = [n, d]
+        self._publicKey = [n, e]
+
+
 table = [
     [' ', ' ', ' '],
     [' ', ' ', ' '],
@@ -94,4 +182,12 @@ def play():
 
 
 if __name__ == '__main__':
+    rsa = RsaCryptography()
+
+    rsa.calculate_rsa_variables()
+    a = rsa.encrypt('test123')
+    b = rsa.decrypt(a)
+
+    print(a, b)
+
     play()
