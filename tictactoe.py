6e598d16c5d253ef7250b78dbc2b9073eef960e9
heitorpagotto <58671082+heitorpagotto@users.noreply.github.com>
2025-05-13 22:23:26 -0300

    adapted tictactoe to be 8 places and rsa class to have a ctor

--- tictactoe.py
+++ tictactoe.py
@@ -5,6 +5,11 @@ class RsaCryptography:
     _privateKey = []
     _publicKey = []
 
+    def __init__(self):
+        self._privateKey = []
+        self._publicKey = []
+        self._calculate_rsa_variables()
+
     @staticmethod
     def _is_prime(n):
         if n == 1:
@@ -35,7 +40,6 @@ class RsaCryptography:
     def encrypt(self, message):
         # 1 - E
         # 0 - N
-        # print(self.pu)
         encoded_message = [ord(char) for char in message]
         encrypted_message = [pow(ch, self._publicKey[1], self._publicKey[0]) for ch in encoded_message]
 
@@ -49,17 +53,17 @@ class RsaCryptography:
 
         return decrypted_message
 
-    def calculate_rsa_variables(self):
-        initial_primes = self._generate_prime_numbers(1000, 10000)
+    def _calculate_rsa_variables(self):
+        min_random_prime = 0
+        max_random_prime = 100
+
+        initial_primes = self._generate_prime_numbers(min_random_prime, max_random_prime)
 
         p = initial_primes[random.randint(0, len(initial_primes) - 1)]
 
-        initial_primes = self._generate_prime_numbers(p + 1, 10000)
+        initial_primes = self._generate_prime_numbers(p + 1, max_random_prime)
 
         q = initial_primes[random.randint(0, len(initial_primes) - 1)]
-
-        # p = 3
-        # q = 11
 
         while q == p:
             q = initial_primes[random.randint(0, len(initial_primes) - 1)]
@@ -67,20 +71,10 @@ class RsaCryptography:
         n = p * q
         delta = (p - 1) * (q - 1)
 
-        print("P= ", p)
-        print("Q= ", q)
-        print("N= ", n)
-        print("Delta= ", delta)
-
         initial_primes = self._generate_prime_numbers(q + 1, delta)
 
-        #aux = "DIGITE UM NUMERO mdc(" + str(delta) + ",e) =1 ; 1 < e < " + str(delta) + ":"
-       # e = initial_primes[random.randint(0, len(initial_primes) - 1)]
-        e = 17
+        e = initial_primes[random.randint(0, len(initial_primes) - 1)]
         d = self._mod_inverse(e, delta)
-
-        print("E= ", e)
-        print("D= ", d)
 
         self._privateKey = [n, d]
         self._publicKey = [n, e]
@@ -150,17 +144,17 @@ def play():
     while playingGame:
         printTable()
         print(f"Vez do jogador: {currentPlayer}")
-        gridPlace = 0
+        gridPlace = -1
 
-        while gridPlace < 1 or gridPlace > 9:
+        while gridPlace < 0 or gridPlace > 8:
             try:
-                gridPlace = int(input("Digite um número de 1 a 9: "))
+                gridPlace = int(input("Digite um número de 0 a 8: "))
             except ValueError:
-                print("Entrada invalida. Digite um numero de 1 a 9.")
+                print("Entrada invalida. Digite um numero de 0 a 8.")
                 continue
 
-        line = (gridPlace - 1) // 3
-        column = (gridPlace - 1) % 3
+        line = 0 if gridPlace == 0 else gridPlace // 3
+        column = 0 if gridPlace == 0 else gridPlace % 3
 
         if table[line][column] != ' ':
             print("Posicao ja ocupada. Escolha outra posição.")
@@ -184,7 +178,6 @@ def play():
 if __name__ == '__main__':
     rsa = RsaCryptography()
 
-    rsa.calculate_rsa_variables()
     a = rsa.encrypt('test123')
     b = rsa.decrypt(a)
 
